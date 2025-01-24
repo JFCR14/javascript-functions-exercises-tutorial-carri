6d8694811b7094cf58a095c6afce82a13732bbc1
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-24 20:15:39 +0000

    1.0

--- exercises/08-Function-parameters/app.js
+++ exercises/08-Function-parameters/app.js
@@ -1,5 +1,7 @@
 // Your code goes here:
-
+function renderPerson(name, date, color, years, sex){
+    return `${name} is a ${years} years old ${sex} born in ${date} with ${color} eyes.`;
+}
 
 
 ///*** Do not edit below this line ***///
