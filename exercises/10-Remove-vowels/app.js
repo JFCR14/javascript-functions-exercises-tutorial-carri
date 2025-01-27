4942eee0b1563909570eeda6c05f6379c577cea2
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-27 18:48:30 +0000

    1.0

--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,4 +1,15 @@
-// Your code goes here
+const rapid = (str) => {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (!"aeiouAEIOU".includes(char)) {
+            result += char.toUpperCase();
+        }
+    }
+    return result;  
+    
+}
+    
 
 
 // Work above this line; do not change code below
